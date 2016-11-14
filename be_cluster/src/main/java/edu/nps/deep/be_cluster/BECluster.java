29e2e93f4423369790f1ba2407d1dbb12b5d45e1
Bruce Allen <bdallen@nps.edu>
2016-11-14 11:56:10 -0800

    create be_cluster_hist_test

---
+++ be_cluster/src/main/java/edu/nps/deep/be_cluster/BECluster.java
@@ -0,0 +1,311 @@
+// based loosely on Spark examples and
+// http://spark.apache.org/docs/latest/programming-guide.html
+
+package edu.nps.deep.be_cluster;
+
+import java.io.IOException;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import scala.Tuple2;
+
+public final class BECluster {
+
+  // ************************************************************
+  // ByteHistogram contains a histogram distribution of bytes.
+  // ************************************************************
+  static class ByteHistogram implements java.io.Serializable {
+    public long[] histogram = new long[256];
+
+    public ByteHistogram() {
+      histogram = new long[256];
+    }
+
+    public void add(char[] chars, int count) {
+      for (int i=0; i<count; i++) {
+        ++histogram[chars[i]];
+      }
+    }
+
+    public void add(ByteHistogram other) { 
+      for (int i = 0; i< histogram.length; i++) {
+        histogram[i] += other.histogram[i];
+      }
+    }
+
+    public String toString() {
+      StringBuilder b = new StringBuilder();
+      long total = 0;
+      for (int i=0; i<256; i++) {
+        b.append(i);
+        b.append(" ");
+        b.append(histogram[i]);
+        b.append(" ");
+        total += histogram[i];
+      }
+      b.append("\n");
+      b.append("total: ");
+      b.append(total);
+      b.append("\n");
+      return b.toString();
+    }
+
+/*
+    public String toString() {
+      StringBuilder b = new StringBuilder();
+      long total = 0;
+      for (int i=0; i<256; i++) {
+        b.append(i);
+        b.append(" ");
+        b.append(histogram[i]);
+        b.append("\n");
+        total += histogram[i];
+      }
+      b.append("total: ");
+      b.append(total);
+      return b.toString();
+    }
+*/
+  }
+
+  // ************************************************************
+  // SplitFileReacordReader reads the requested split and returns ByteHistogram.
+  // ref. https://github.com/apache/mahout/blob/master/integration/src/main/java/org/apache/mahout/text/wikipedia/XmlInputFormat.java
+  // ************************************************************
+  static class SplitFileRecordReader
+                            extends org.apache.hadoop.mapreduce.RecordReader<
+                            Long, ByteHistogram> {
+
+    private SplitReader splitReader;
+    private ByteHistogram byteHistogram = new ByteHistogram();
+    private long splitNumber = 0;
+    private boolean isDone = false;
+
+    @Override
+    public void initialize(
+                 org.apache.hadoop.mapreduce.InputSplit split,
+                 org.apache.hadoop.mapreduce.TaskAttemptContext context)
+                        throws IOException, InterruptedException {
+
+      splitReader = SplitReader.getReader(split, context);
+    }
+
+    @Override
+    public boolean nextKeyValue() throws IOException, InterruptedException {
+      int C_SIZE = 50000;
+      char[] c = new char[C_SIZE];
+
+      // done when no more bytes to read in split
+      if (isDone) {
+        return false;
+      }
+
+      // key
+      ++splitNumber;
+
+/*
+      // value
+      while (true) {
+        final int count = splitReader.read(c, 0, C_SIZE);
+        if (count == -1) {
+          break;
+        }
+        if (count == 0) {
+          throw new IOException("Invalid value: count=0");
+        }
+        byteHistogram.add(c, count);
+      }
+*/
+
+      // value
+      long splitDistance = splitReader.splitDistance();
+      while (splitDistance > 0) {
+        int count = (splitDistance > C_SIZE) ? C_SIZE : (int)splitDistance;
+        final int countRead = splitReader.read(c, 0, count);
+        if (countRead != count) {
+          throw new IOException("Unexpected count: " + count + ", countRead: " + countRead);
+        }
+
+        byteHistogram.add(c, count);
+        splitDistance -= count;
+      }
+
+      // done with this partition
+      isDone = true;
+      return true;
+    }
+
+    @Override
+    public Long getCurrentKey() throws IOException, InterruptedException {
+      return new Long(splitNumber);
+    }
+
+    @Override
+    public ByteHistogram getCurrentValue()
+                                  throws IOException, InterruptedException {
+      return byteHistogram;
+    }
+
+    @Override
+    public float getProgress() throws IOException {
+      return (isDone == true) ? 1.0f : 0.0f;
+    }
+
+    @Override
+    public void close() throws IOException {
+      splitReader.close();
+    }
+  }
+
+  // ************************************************************
+  // SplitFileInputFormat implements createRecordReader which returns
+  // SplitFileRecordReader for calculating ByteHistogram for one split.
+  // ************************************************************
+  public static class SplitFileInputFormat
+        extends org.apache.hadoop.mapreduce.lib.input.FileInputFormat<
+                         Long, ByteHistogram> {
+
+    // createRecordReader returns SplitFileRecordReader
+    @Override
+    public org.apache.hadoop.mapreduce.RecordReader<
+                         Long, ByteHistogram>
+           createRecordReader(
+                 org.apache.hadoop.mapreduce.InputSplit split,
+                 org.apache.hadoop.mapreduce.TaskAttemptContext context)
+                       throws IOException, InterruptedException {
+
+      SplitFileRecordReader reader = new SplitFileRecordReader();
+      reader.initialize(split, context);
+      return reader;
+    }
+  }
+
+  // ************************************************************
+  // Main
+  // ************************************************************
+
+  public static void main(String[] args) {
+
+    if (args.length != 1) {
+      System.err.println("Usage: BECluster <input path>");
+      System.exit(1);
+    }
+
+    // set up the Spark Configuration
+    SparkConf sparkConfiguration = new SparkConf();
+    sparkConfiguration.setAppName("Spark Byte Count App");
+    sparkConfiguration.set("log4j.logger.org.apache.spark.rpc.akka.ErrorMonitor", "FATAL");
+    sparkConfiguration.set("log4j.logger.org.apache.spark.scheduler.DAGScheduler", "TRACE");
+    sparkConfiguration.set("yarn.log-aggregation-enable", "true");
+    sparkConfiguration.set("fs.hdfs.impl.disable.cache", "true");
+    sparkConfiguration.set("spark.app.id", "Spark Byte Count 2 App");
+    sparkConfiguration.set("spark.executor.extrajavaoptions", "-XX:+UseConcMarkSweepGC");
+    sparkConfiguration.set("spark.dynamicAllocation.maxExecutors", "10000");
+
+// no, we will have multiple keys:    sparkConfiguration.set("spark.default.parallelism", "1");
+    sparkConfiguration.set("spark.default.parallelism", "64");
+
+    sparkConfiguration.set("spark.driver.maxResultSize", "8g"); // default 1g, may use 2.5g
+
+    // set up the Spark context
+    JavaSparkContext sparkContext = new JavaSparkContext(sparkConfiguration);
+
+    // several hadoop functions return IOException
+    try {
+
+      // get the hadoop job
+      Job hadoopJob = Job.getInstance(
+                       sparkContext.hadoopConfiguration(), "BECluster");
+
+      // get the file system
+      FileSystem fileSystem =
+                       FileSystem.get(sparkContext.hadoopConfiguration());
+
+      // get the input path
+      Path inputPath = new Path(args[0]);
+
+      // iterate over files under the input path
+      RemoteIterator<LocatedFileStatus> fileStatusListIterator =
+                                       fileSystem.listFiles(inputPath, true);
+      int i = 0;
+      long totalBytes = 0;
+      while (fileStatusListIterator.hasNext()) {
+
+        // get file status for this file
+        LocatedFileStatus locatedFileStatus = fileStatusListIterator.next();
+
+//        // restrict number of files to process or else comment this out
+//        if (++i > 10) {
+//          break;
+//        }
+//if (locatedFileStatus.getPath().toString().indexOf("Fedora-Xfce-Live-x86_64-24-1.2.iso") >= 0) {
+//continue;
+//}
+
+        System.out.println("adding " + locatedFileStatus.getLen() +
+                  " bytes at path " + locatedFileStatus.getPath().toString());
+
+
+        // add this file
+        org.apache.hadoop.mapreduce.lib.input.FileInputFormat.addInputPath(
+                                   hadoopJob, locatedFileStatus.getPath());
+
+        totalBytes += locatedFileStatus.getLen();
+      }
+      System.out.println("total bytes added: " + totalBytes);
+
+      // create the RDD of byte histograms for splits
+      JavaPairRDD<Long, ByteHistogram> rdd = sparkContext.newAPIHadoopRDD(
+               hadoopJob.getConfiguration(),        // configuration
+               SplitFileInputFormat.class,          // F
+               Long.class,                          // K
+               ByteHistogram.class);                // V
+
+      // reduce RDD to total result
+      Tuple2<Long, ByteHistogram> histogramTotalTuple = rdd.reduce(
+            new Function2<Tuple2<Long, ByteHistogram>,
+                         Tuple2<Long, ByteHistogram>,
+                         Tuple2<Long, ByteHistogram>>() {
+        @Override
+        public Tuple2<Long, ByteHistogram> call(
+                                Tuple2<Long, ByteHistogram> v1,
+                                Tuple2<Long, ByteHistogram> v2) {
+
+          // add second byteHistogram to first
+          v1._2().add(v2._2());
+          return new Tuple2<Long, ByteHistogram>(new Long(1), v1._2());
+        }
+      });
+
+      // show histogram total
+      System.out.println("Histogram total:\n" +
+                         histogramTotalTuple._2());
+
+      // save total in text file
+      java.io.File totalFile = new java.io.File("temp_total_textfile");
+      try {
+        java.io.BufferedWriter out = new java.io.BufferedWriter(new java.io.FileWriter(totalFile));
+        out.write("Histogram total:\n" +
+                         histogramTotalTuple._2());
+        out.close();
+      } catch (Exception e) {
+        System.out.println("Error: Failure saving " + totalFile.toString());
+      }
+
+    }  catch (IOException e) {
+      System.err.println("Error starting main: " + e);
+      System.exit(1);
+    }
+  }
+}
+
---
+++ be_cluster/src/main/java/edu/nps/deep/be_cluster/SplitReader.java
@@ -0,0 +1,217 @@
+package edu.nps.deep.be_cluster;
+
+/*
+import java.io.IOException;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import scala.Tuple2;
+*/
+
+import java.io.IOException;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IOUtils;
+
+/**
+ * Reader interface for reading char[n] from hadoop file bytes.
+ * Limitation: It is an IOException to read more than MAX_BUFSIZE at once.
+ *
+ * Note: This reads past the hadoop split.  Call splitDistance() to see
+ *       how many more bytes can be read before reaching the split.
+ */
+public final class SplitReader extends java.io.Reader {
+
+  private final InputSplit inputSplit;
+  private final TaskAttemptContext taskAttemptContext;
+
+  // Hadoop input stream
+  private FSDataInputStream in;
+
+  // buffer between Hadoop and SplitReader output
+  private static final int MAX_BUFSIZE = 131072; // 2^17=128KiB
+  private long moreFile;
+  private long moreSplit;
+  private byte[] buffer;
+  private int bufferHead;
+
+  private SplitReader(InputSplit split,
+                     TaskAttemptContext context) {
+    inputSplit = split;
+    taskAttemptContext = context;
+  }
+
+  // open and return a FSDataInputStream
+  private void openIN() throws IOException, InterruptedException {
+
+    // open the input file
+    final Path path = ((FileSplit)inputSplit).getPath();
+    final Configuration configuration = taskAttemptContext.getConfiguration();
+    final FileSystem fileSystem = path.getFileSystem(configuration);
+    in = fileSystem.open(path);
+  }
+
+  // length of file
+  private long getFileLen() throws IOException, InterruptedException {
+    final Path path = ((FileSplit)inputSplit).getPath();
+    final Configuration configuration = taskAttemptContext.getConfiguration();
+    final FileSystem fileSystem = path.getFileSystem(configuration);
+    final long fileLen = fileSystem.getFileStatus(path).getLen();
+    return fileLen;
+  }
+
+  // get a reader compatible with java.io.Reader
+  public static SplitReader getReader(InputSplit split,
+                                      TaskAttemptContext context)
+                               throws IOException, InterruptedException {
+
+    // create the reader to return
+    SplitReader reader = new SplitReader(split, context);
+
+    // get offset to start of split
+    final long start = ((FileSplit)reader.inputSplit).getStart();
+
+    // open the reader
+    reader.openIN();
+
+    // seek to the split
+    reader.in.seek(start);
+
+    // set initial values
+    reader.moreFile = reader.getFileLen() - start;
+    if (reader.moreFile < 0) {
+      throw new IOException("invalid state");
+    }
+    reader.moreSplit = ((FileSplit)reader.inputSplit).getLength();
+    reader.buffer = new byte[MAX_BUFSIZE];
+    reader.bufferHead = MAX_BUFSIZE;
+
+    return reader;
+  }
+
+  // read into buffer if it is too empty
+  private void prepareBuffer(int sizeRequested)
+                               throws IOException {
+//                               throws IOException, InterruptedException {
+
+    // no action if data is available
+    int sizeAvailable = MAX_BUFSIZE - bufferHead;
+    if (sizeRequested <= sizeAvailable) {
+      return;
+    }
+
+    // no action if at EOF
+    if (moreFile == 0) {
+      System.err.println("Note: no prepareBuffer read because moreFile is 0");
+      return;
+    }
+
+    // impose a max read size less than buffer size
+    if (sizeRequested > MAX_BUFSIZE / 2) {
+      throw new IOException("invalid state");
+    }
+
+    // get count of bytes to read
+    int countToRead = bufferHead;
+    if (countToRead > moreFile) {
+      countToRead = (int)moreFile;
+    }
+
+    // shift unread bytes from end to left of bytes to read
+    for (int i=bufferHead; i<MAX_BUFSIZE; i++) {
+      buffer[i-countToRead] = buffer[i];
+    }
+
+    // read count of bytes into buffer
+    IOUtils.readFully(in, buffer, MAX_BUFSIZE - countToRead, countToRead);
+
+//System.out.println("prepareBuffer.readFully: bufferHead: " + bufferHead + ", countToRead: " + countToRead);
+
+    // adjust tracking variables
+    moreFile -= countToRead;
+    moreSplit -= countToRead; // goes negative when reading beyond split
+    bufferHead -= countToRead;
+
+//for (int j=bufferHead; j<MAX_BUFSIZE; j++) {
+//System.out.println("prepareBuffer.buffer[" + j + "]: " + buffer[j]);
+//}
+
+  }
+
+  // ************************************************************
+  // public Reader interfaces
+  // ************************************************************
+
+  // specialized interface
+  /**
+   * splitDistance becomes 0 and goes negative as we begin to read into
+   * the next split.
+   */
+  public long splitDistance() {
+    return moreSplit - (MAX_BUFSIZE - bufferHead);
+  }
+
+  // close
+//zz  public void close() throws IOException, InterruptedException {
+  public void close() throws IOException {
+    IOUtils.closeStream(in);
+//    in.close();
+  }
+
+  // do not support marking
+  public boolean markSupported() {
+    return false;
+  }
+
+  public int read(char[] c, int off, int len)
+//                      throws IOException, InterruptedException {
+                      throws IOException {
+//System.out.println("stdout: read: off: " + off + ", len: " + len);
+//System.err.println("stderr: read: off: " + off + ", len: " + len);
+
+    // require a max read size less than buffer size
+    if (len > MAX_BUFSIZE / 2) {
+      throw new IOException("invalid usage: " + len +
+                            " must be less than " + MAX_BUFSIZE / 2 + ".");
+    }
+
+    // make sure buffer is ready to support the read
+    prepareBuffer(len);
+
+    // get less than len if at EOF
+    final int count = (len < MAX_BUFSIZE - bufferHead) ? len :
+                                               MAX_BUFSIZE - bufferHead;
+
+    // no count means EOF
+    if (count == 0) {
+      return -1;
+    }
+
+    // read count from buffer
+    for (int i=0; i<count; i++) {
+      c[off + i] = (char)(0xff & buffer[bufferHead + i]);
+    }
+
+    // move the buffer head forward
+    bufferHead += count;
+
+    return count;
+  }
+}
+
