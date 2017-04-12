dc4692140d6a3377dde1c71beb701d5b568313a3
Bruce Allen <bdallen@nps.edu>
2017-04-12 10:12:56 -0700

    add analysis

---
+++ python/calculate_flex_bytes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Calculate flex bytes considered and accepted from the log file.
+from argparse import ArgumentParser
+
+def read_totals(logfile):
+    flex_bytes_considered = 0
+    flex_bytes_accepted = 0
+
+    with open(logfile, 'r') as infile:
+        for line in infile:
+            if line[:22] == "flex_bytes_considered ":
+                flex_bytes_considered += int(line[22:])
+            elif line[:20] == "flex_bytes_accepted ":
+                flex_bytes_accepted += int(line[20:])
+
+    return flex_bytes_considered, flex_bytes_accepted
+
+
+if __name__=="__main__":
+
+    parser = ArgumentParser(prog='calculate_flex_bytes.py',
+                  description='Calculate flex bytes considered and accepted from the log file')
+    parser.add_argument('--logfile', help= 'Yarn log file',
+                        default='w')
+    args = parser.parse_args() 
+    logfile = args.logfile
+
+    flex_bytes_considered, flex_bytes_accepted = read_totals(logfile)
+
+    print("flex_bytes_considered: %d" % flex_bytes_considered)
+    print("flex_bytes_accepted: %d" % flex_bytes_accepted)
+
