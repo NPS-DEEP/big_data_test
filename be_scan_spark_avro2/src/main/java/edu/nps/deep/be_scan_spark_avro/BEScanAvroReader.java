e5d2a47a68f06653555ec444c84316ed747d2c62
Bruce Allen <bdallen@nps.edu>
2017-07-11 17:42:44 -0700

    add scan using splits

---
+++ be_scan_spark_avro2/src/main/java/edu/nps/deep/be_scan_spark_avro/BEScanAvroReader.java
@@ -0,0 +1,116 @@
+// based loosely on Spark examples and
+// http://spark.apache.org/docs/latest/programming-guide.html
+
+package edu.nps.deep.be_scan_spark;
+
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.SparkFiles;
+import scala.Tuple2;
+
+// See be_scan/java_bindings/Tests.java for example usage of the be_scan API.
+import edu.nps.deep.be_scan.BEScan;
+
+/**
+ * Scans and imports all artifacts at the first call to nextKeyValue().
+ * Nothing meaningful is returned for the RDD, and returned constants
+ * should be changed to null.
+ */
+public final class BEScanAvroReader
+                         extends org.apache.hadoop.mapreduce.RecordReader<
+                         Long, String> {
+
+  // static scan engine and scanner
+  private static final edu.nps.deep.be_scan.ScanEngine scanEngine;
+  private static final edu.nps.deep.be_scan.Scanner scanner;
+  static {
+    System.load(SparkFiles.get("libstdc++.so"));
+    System.load(SparkFiles.get("libicudata.so"));
+    System.load(SparkFiles.get("libicuuc.so"));
+    System.load(SparkFiles.get("liblightgrep.so"));
+    System.load(SparkFiles.get("liblightgrep_wrapper.so"));
+    System.load(SparkFiles.get("libbe_scan.so"));
+    System.load(SparkFiles.get("libbe_scan_jni.so"));
+
+    scanEngine = new edu.nps.deep.be_scan.ScanEngine("email");
+    scanner = new edu.nps.deep.be_scan.Scanner(scanEngine, "unused output filename");
+  }
+
+  private final String filename;
+  private boolean isParsed = false;
+  private BufferRecordReader reader;
+
+  @Override
+  public void initialize(InputSplit split, TaskAttemptContext context)
+                                throws IOException, InterruptedException {
+
+    // open the reader
+    reader = new BufferRecordReader(split, context);
+
+    // get the filename string for reporting artifacts
+    filename = ((FileSplit)inputSplit).getPath().toString();
+  }
+
+
+  @Override
+  public boolean nextKeyValue() throws IOException, InterruptedException {
+
+    // call exactly once
+    if (isParsed) {
+      throw new IOException("Error: BESanAvroReader next already called");
+    }
+
+    // parse all records in the split
+    while(reader.hasNext()) {
+      BufferRecord record = reader.next();
+
+      // scan the buffer
+      String success = scanner.scan(
+                          filename,
+                          java.math.BigInteger.valueOf(record.offset),
+                          "", record.buffer, record.buffer.length);
+      if (!success.equals("")) {
+        throw new IOException("Error in scan: '" + success + "'");
+      }
+    }
+
+    // done
+    return false;
+  }
+
+  @Override
+  public Long getCurrentKey() throws IOException, InterruptedException {
+    return new Long(1);
+  }
+
+  @Override
+  public String getCurrentValue() throws IOException, InterruptedException {
+    return "done";
+  }
+
+  @Override
+  public float getProgress() throws IOException {
+    return isParsed ? 1.0f : 0.0f;
+  }
+
+  @Override
+  public void close() throws IOException {
+    // no action
+  }
+}
+
---
+++ be_scan_spark_avro2/src/main/java/edu/nps/deep/be_scan_spark_avro/BufferRecordReader.java
@@ -0,0 +1,133 @@
+package edu.nps.deep.be_scan_spark;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IOUtils;
+
+import org.apache.avro.file.SeekableInput;
+import org.apache.avro.mapred.FsInput;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.mapred.FsInput;
+
+
+/**
+ * Reads Avro records of media image data
+ */
+public final class BufferRecordReader {
+
+  /**
+   * The Avro media image schema.
+   */
+  public final class BufferRecord {
+
+    public final long offset;
+    public final byte[] buffer;
+
+    public BufferRecord(long p_offset, byte[] p_buffer) {
+      offset = p_offset;
+      buffer = p_buffer;
+    }
+  }
+
+  /**
+   * The Avro media image schema.
+   */
+  private static final String imageSchemaString =
+    "{" +
+     "\"namespace\": \"edu.nps.deep.be_scan_spark\"," +
+     "\"type\": \"record\"," +
+     "\"name\": \"AvroSlice\"," +
+     "\"fields\": [" +
+       "{\"name\": \"offset\", \"type\": \"long\"}," +
+       "{\"name\": \"data\", \"type\": \"bytes\"}" +
+     "]" +
+    "}";
+
+  private static final org.apache.avro.Schema imageSchema = new
+     org.apache.avro.Schema.Parser().parse(imageSchemaString);
+
+  private static final DatumReader<GenericRecord> datumReader = new
+     GenericDatumReader<GenericRecord>(imageSchema);
+
+
+//  private final InputSplit inputSplit;
+//  private final TaskAttemptContext taskAttemptContext;
+
+  public final Configuration configuration;
+  public final String filename;
+  public final long fileSize;
+  public final long splitStart;
+  public final long splitSize;
+  public final byte[] buffer;
+  private GenericRecord genericRecord; // object reuse
+
+  public BufferRecordReader(InputSplit inputSplit,
+                            TaskAttemptContext taskAttemptContext)
+                               throws IOException, InterruptedException {
+
+    // configuration
+    configuration = taskAttemptContext.getConfiguration();
+
+    // hadoop path
+    final Path path = ((FileSplit)inputSplit).getPath();
+
+    // hadoop filesystem
+    final FileSystem fileSystem = path.getFileSystem(configuration);
+
+    // filename
+    filename = path.toString();
+
+    // fileSize
+    fileSize = fileSystem.getFileStatus(path).getLen();
+
+    // splitStart
+    splitStart = ((FileSplit)inputSplit).getStart();
+
+    // splitSize
+    splitSize = ((FileSplit)inputSplit).getLength();
+
+    // open the Avro file
+    FsInput fsInput = new FsInput(path, configuration);
+    DataFileReader<GenericRecord> reader = new DataFileReader(
+                                                   fsInput, datumReader);
+
+    // move to first sync in split
+    reader.sync(splitStart);
+  }
+
+  // more if not EOF and not past split
+  public boolean hasNext() {
+    if (!reader.hasNext()) {
+      return false;
+    }
+    return !(reader.pastSync(splitStart + splitSize));
+  }
+
+  // read next.  Error if request to start after end of split.
+  public BufferRecord next() throws IOException {
+    if (!hasNext()) {
+      throw new IOException("Error in SplitReader.next: next not available");
+    }
+
+    // read the Avro record
+    genericRecord = reader.next();
+    long offset = (long)avroSlice.get("offset");
+    byte[] buffer = ((ByteBuffer)avroSlice.get("data")).array();
+
+    // return BufferRecord
+    return new BufferRecord(offset, buffer);
+  }
+}
+
