aa9d4b4cfc26819766ab1d5ac1105d2f605f0bec
Bruce Allen <bdallen@nps.edu>
2017-08-03 17:06:30 -0700

    add be_scan_spark_hbase

---
+++ be_scan_spark_hbase/src/main/java/edu/nps/deep/be_scan_spark_hbase/BEScanSparkHbase.java
@@ -0,0 +1,166 @@
+package edu.nps.deep.be_scan_spark2;
+
+import java.io.IOException;
+//import java.io.File;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.Date;
+import java.util.Iterator;
+import java.text.SimpleDateFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.conf.Configuration;
+
+import org.apache.spark.broadcast.Broadcast;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaFutureAction;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.VoidFunction;
+import scala.Tuple2;
+
+import org.apache.hadoop.hbase.client.Put
+
+public final class BEScanSpark2 {
+
+  // ************************************************************
+  // BEScanRawFileInputFormat implements createRecordReader which returns
+  // BEScanSplitReader which stores features instead of returning them.
+  // ************************************************************
+  public static class BEScanRawFileInputFormat
+        extends org.apache.hadoop.mapreduce.lib.input.FileInputFormat<
+                         Long, String> {
+
+    // createRecordReader returns EmailReader
+    @Override
+    public org.apache.hadoop.mapreduce.RecordReader<Long, String>
+           createRecordReader(
+                 org.apache.hadoop.mapreduce.InputSplit split,
+                 org.apache.hadoop.mapreduce.TaskAttemptContext context)
+                       throws IOException, InterruptedException {
+
+      BEScanSplitReader reader = new BEScanSplitReader();
+      reader.initialize(split, context);
+      return reader;
+    }
+  }
+
+  // ************************************************************
+  // Main
+  // ************************************************************
+
+  public static void main(String[] args) {
+
+    if (args.length != 2) {
+      System.err.println("Usage: BEScanSpark2 <directory holding .so files> <input path>");
+      System.exit(1);
+    }
+
+    // set up the Spark Configuration
+    SparkConf sparkConfiguration = new SparkConf();
+    sparkConfiguration.setAppName("be_scan Split 2");
+    sparkConfiguration.set("log4j.logger.org.apache.spark.rpc.akka.ErrorMonitor", "FATAL");
+    sparkConfiguration.set("log4j.logger.org.apache.spark.scheduler.DAGScheduler", "TRACE");
+    sparkConfiguration.set("yarn.log-aggregation-enable", "true");
+    sparkConfiguration.set("fs.hdfs.impl.disable.cache", "true");
+    sparkConfiguration.set("spark.app.id", "BEScanSparkAvro App");
+//    sparkConfiguration.set("spark.executor.extrajavaoptions", "-XX:+UseConcMarkSweepGC");
+    sparkConfiguration.set("spark.dynamicAllocation.maxExecutors", "400");
+
+    sparkConfiguration.set("spark.default.parallelism", "1");
+
+    sparkConfiguration.set("spark.driver.maxResultSize", "100g"); // default 1g
+
+    sparkConfiguration.set("spark.yarn.executor.memoryOverhead", "4000"); // default 1g
+
+    // set up the Spark context
+    JavaSparkContext sparkContext = new JavaSparkContext(sparkConfiguration);
+
+    // make .so libraries available on each node
+//    sparkContext.addFile("/opt/gcc/5.2.0/lib64/libstdc++.so");
+    sparkContext.addFile(args[0] + "64/" + "libstdc++.so");
+    sparkContext.addFile(args[0] + "/" + "libicudata.so");
+    sparkContext.addFile(args[0] + "/" + "libicuuc.so");
+    sparkContext.addFile(args[0] + "/" + "liblightgrep.so");
+    sparkContext.addFile(args[0] + "/" + "liblightgrep_wrapper.so");
+    sparkContext.addFile(args[0] + "/" + "libbe_scan.so");
+    sparkContext.addFile(args[0] + "/" + "libbe_scan_jni.so");
+
+    try {
+
+      // get the hadoop job
+      Job hadoopJob = Job.getInstance(sparkContext.hadoopConfiguration(),
+                    "BEScanSpark2 job");
+
+      // get the hadoop job configuration object
+      Configuration configuration = hadoopJob.getConfiguration();
+
+      // set the HBase table name in the configuration
+      configuration.set(TableOutputFormat.OUTPUT_TABLE, "rdc_artifacts");
+
+      // get the file system
+      FileSystem fileSystem =
+                       FileSystem.get(sparkContext.hadoopConfiguration());
+
+      // get the input path
+      Path inputPath = new Path(args[1]);
+
+      // iterate over files under the input path to schedule files
+      RemoteIterator<LocatedFileStatus> fileStatusListIterator =
+                                       fileSystem.listFiles(inputPath, true);
+      int i = 0;
+      long totalBytes = 0;
+      while (fileStatusListIterator.hasNext()) {
+
+        // get file status for this file
+        LocatedFileStatus locatedFileStatus = fileStatusListIterator.next();
+
+        // restrict number of files to process else comment this out
+        if (++i > 2) {
+          break;
+        }
+
+        // show this file being added
+        System.out.println("adding " + locatedFileStatus.getLen() +
+                  " bytes at path " + locatedFileStatus.getPath().toString());
+
+        // add this file to the job
+        FileInputFormat.addInputPath(hadoopJob, locatedFileStatus.getPath());
+        totalBytes += locatedFileStatus.getLen();
+
+//        // stop after some amount
+//        if (totalBytes > 3510000000000L) {
+//          break;
+//        }
+      }
+
+      // Transformation: create the pairRDD for all the files and splits
+      JavaPairRDD<Long, Put> pairRDD = sparkContext.newAPIHadoopRDD(
+               configuration,                        // configuration
+               BEScanRawFileInputFormat.class,       // F
+               Long.class,                           // K
+               Put.class);                           // V
+
+      // save the artifacts in the RDD to HBase
+      pairRDD.saveAsNewAPIHadoopDataset(configuration);
+
+      // show the total bytes processed
+      System.out.println("total bytes processed: " + totalBytes);
+
+      // Done
+      System.out.println("Done.");
+
+    }  catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
+
---
+++ be_scan_spark_hbase/src/main/java/edu/nps/deep/be_scan_spark_hbase/BEScanSplitReader.java
@@ -0,0 +1,165 @@
+// based loosely on Spark examples and
+// http://spark.apache.org/docs/latest/programming-guide.html
+
+package edu.nps.deep.be_scan_spark2;
+
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.SparkFiles;
+import scala.Tuple2;
+
+import edu.nps.deep.be_scan.Artifact;
+
+/**
+ * Scans and imports all artifacts at the first call to nextKeyValue().
+ * Nothing meaningful is returned for the RDD, and returned constants
+ * should be changed to null.
+ */
+public final class BEScanSplitReader
+                         extends org.apache.hadoop.mapreduce.RecordReader<
+                         Long, String> {
+
+  // static scan engine and scanner
+  private static final edu.nps.deep.be_scan.ScanEngine scanEngine;
+  private static final edu.nps.deep.be_scan.Scanner scanner;
+  static {
+    System.load(SparkFiles.get("libstdc++.so"));
+    System.load(SparkFiles.get("libicudata.so"));
+    System.load(SparkFiles.get("libicuuc.so"));
+    System.load(SparkFiles.get("liblightgrep.so"));
+    System.load(SparkFiles.get("liblightgrep_wrapper.so"));
+    System.load(SparkFiles.get("libbe_scan.so"));
+    System.load(SparkFiles.get("libbe_scan_jni.so"));
+
+    scanEngine = new edu.nps.deep.be_scan.ScanEngine("email");
+    scanner = new edu.nps.deep.be_scan.Scanner(scanEngine);
+  }
+
+  private String filename;
+  private BufferReader reader;
+  byte[] previous_buffer = null;
+  private Queue<Artifact> artifacts = new Queue<Artifact>();
+  private Artifact artifact = null;
+
+/*
+  // consume artifacts, change this to DB as desired
+  void consumeArtifacts() {
+    while (!scanner.empty()) {
+      edu.nps.deep.be_scan.Artifact artifact = scanner.get();
+      // for now, just print it
+      System.out.println(artifact.toString());
+    }
+  }
+*/
+
+  @Override
+  public void initialize(InputSplit split, TaskAttemptContext context)
+                                throws IOException, InterruptedException {
+
+    // open the reader
+    reader = new BufferReader(split, context);
+
+    // get the filename string for reporting artifacts
+    filename = ((FileSplit)split).getPath().toString();
+
+    // set up to read this split, no recursion prefix
+    scanner.scanSetup(filename, "");
+
+    // done if no data
+    if (!reader.hasNext()) {
+      // done
+      return;
+    }
+
+    // read the first buffer
+    previous_buffer = null;
+    BufferReader.BufferRecord record = reader.next();
+
+    // scan the first buffer
+    String success = scanner.scan(record.offset,
+                                  previous_buffer, record.buffer);
+    if (!success.equals("")) {
+      throw new IOException("Error: " + success);
+    }
+
+    // move current buffer to previous buffer
+    previous_buffer = record.buffer;
+  }
+
+  private void scanAsNeeded() throws IOException {
+
+    // scan buffers until we get artifacts
+    while (reader.hasNext() && artifacts.isEmpty()) {
+
+      // read next
+      BufferReader.BufferRecord record = reader.next();
+
+      // scan next
+      String success = scanner.scan(record.offset,
+                                    previous_buffer, record.buffer);
+      if (!success.equals("")) {
+        throw new IOException("Error: " + success);
+      }
+
+      // move current buffer to previous buffer
+      previous_buffer = record.buffer;
+    }
+  }
+
+  @Override
+  public boolean nextKeyValue() throws IOException, InterruptedException {
+
+    // maybe scan more
+    scanAsNeeded();
+    artifact = artifacts.poll(); // will be null if empty
+    if (artifact == null) {
+      // done
+      return false;
+    } else {
+      // more
+      return true;
+    }
+  }
+
+  @Override
+  public Long getCurrentKey() throws IOException, InterruptedException {
+    return new Long(1);
+  }
+
+  @Override
+  public Put getCurrentValue() throws IOException, InterruptedException {
+    // build key, value put object for HBase entry
+    Put put = new Put(Bytes.toBytes("email," + filename + "," + 
+                                    artifact.getArtifact()));
+    put.addColumn(Bytes.toBytes("f"),        // column family
+                  Bytes.toBytes(artifact.getFeature()));
+    return put;
+  }
+
+  @Override
+  public float getProgress() throws IOException {
+    return reader.hasNext() ? 1.0f : 0.0f;
+  }
+
+  @Override
+  public void close() throws IOException {
+    // no action
+  }
+}
+
